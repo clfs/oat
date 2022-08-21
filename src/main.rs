ec13f68bbb39a66771b666ef8620f4a2a1f500f0
Calvin Figuereo-Supraner <mail@calvin.page>
2022-08-21 13:08:00 -0700

    Replace BufRead with Read in uci::run

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod uci;
 
 fn main() {
     uci::run(
-        &mut std::io::stdin().lock(),
+        &mut std::io::stdin(),
         &mut std::io::stdout(),
         &mut engine::Engine::new(),
     );
--- src/uci.rs
+++ src/uci.rs
@@ -1,14 +1,16 @@
 use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Read;
 use std::io::Write;
 
 use crate::engine::Engine;
 
 pub fn run<R, W>(reader: &mut R, writer: &mut W, engine: &mut Engine)
 where
-    R: BufRead,
+    R: Read,
     W: Write,
 {
-    for line in reader.lines() {
+    for line in BufReader::new(reader).lines() {
         engine.noop();
         writeln!(writer, "echo: {}", line.unwrap()).unwrap();
     }
