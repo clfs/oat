861e94bf93fc92f5e4f5de7dc8fef15622af9a82
Calvin Figuereo-Supraner <mail@calvin.page>
2022-08-27 08:36:16 -0700

    Add Color impl

--- src/color.rs
+++ src/color.rs
@@ -1,4 +1,19 @@
+#[repr(u8)]
 pub enum Color {
     White,
     Black,
+}
+
+impl Color {
+    pub fn opposite(self) -> Color {
+        match self {
+            Color::White => Color::Black,
+            Color::Black => Color::White,
+        }
+    }
+
+    pub fn new_from_index(i: u8) -> Color {
+        assert!(i < 2);
+        unsafe { std::mem::transmute(i) }
+    }
 }
